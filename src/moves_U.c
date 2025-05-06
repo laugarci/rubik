2b7893a0876c76f18ad6c66fb0b0ed095b48f52e
Laura Garcia Arch <garch.laura@gmail.com>
2025-05-06 10:54:29 +0200

    moves U changed

--- src/moves_U.c
+++ src/moves_U.c
@@ -3,27 +3,31 @@
 
 void move_U(t_cube *c)
 {
-	char tmp[SIZE];
+	char *tmp = malloc(sizeof(char) * SIZE);
+	if (!tmp)
+		return ;
 
 	for (int i = 0; i < SIZE; i++)
 		tmp[i] = c->face[F][i];
 
 	for (int i = 0; i < SIZE; i++)
-		c->face[F][i] = c->face[L][i];
+		c->face[F][i] = c->face[R][i];
 
 	for (int i = 0; i < SIZE; i++)
-		c->face[L][i] = c->face[B][i];
+		c->face[R][i] = c->face[B][SIZE - 1 - i];
 
 	for (int i = 0; i < SIZE; i++)
-		c->face[B][i] = c->face[R][i];
+		c->face[B][i] = c->face[L][SIZE - 1 - i];
 
 	for (int i = 0; i < SIZE; i++)
-		c->face[R][i] = tmp[i];
+		c->face[L][i] = tmp[i];
+
+	free(tmp);
 }
 
-void	move_U_prime(t_cube *c)
+void move_U_prime(t_cube *c)
 {
-	(void)c;
+	(void)c;	
 }
 
 void	move_U2(t_cube *c)
